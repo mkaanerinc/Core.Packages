42c424cd405273d8fd9da38af8522de4b92df0e4
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2025-03-20 18:32:06 +0300

    feat: added generic Entity class with timestamps to Core.Packages
    Added a generic Entity<TId> class to Core.Infrastructure.Persistence.Repositories namespace in Core.Packages project. Includes Id and timestamp properties (CreatedAt, UpdatedAt, DeletedAt) for reusable entity structure across projects.

---
+++ src/Core.Infrastructure/Persistence/Repositories/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Infrastructure.Persistence.Repositories;
+
+public class Entity<TId> : IEntityTimestamps
+{
+    public TId Id { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+
+    public Entity()
+    {
+        Id = default!;
+    }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
---
+++ src/Core.Infrastructure/Persistence/Repositories/IEntityTimestamps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Infrastructure.Persistence.Repositories;
+
+internal interface IEntityTimestamps
+{
+    DateTimeOffset CreatedAt { get; set; }
+    DateTimeOffset? UpdatedAt { get; set; }
+    DateTimeOffset? DeletedAt { get; set; }
+}
