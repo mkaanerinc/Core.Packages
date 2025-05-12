5b7cb1d45aa73d73bf75d953b5ea05b3a4155ea4
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2025-05-12 14:01:36 +0300

    feat(crosscuttingconcerns): Implement LoggingServiceRegistration for logger configuration
    - Added LoggingServiceRegistration extension to register logger services based on LoggerType.
    - Supported FileLogger and MsSqlLogger implementations.
    - Introduced exception handling for unsupported logger types.

---
+++ src/Core.CrossCuttingConcerns/Logging/Extensions/LoggingServiceRegistration.cs
@@ -0,0 +1,41 @@
+﻿using Core.CrossCuttingConcerns.Logging.Models;
+using Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Logging.Extensions;
+
+/// <summary>
+/// Provides extension methods for registering logging services based on the specified logger type.
+/// </summary>
+public static class LoggingServiceRegistration
+{
+    /// <summary>
+    /// Registers the appropriate <see cref="ILoggerService"/> implementation in the dependency injection container 
+    /// based on the specified <see cref="LoggerType"/>.
+    /// </summary>
+    /// <param name="services">The service collection to which the logger service will be added.</param>
+    /// <param name="loggerType">The type of logger to register. Defaults to <see cref="LoggerType.FileLogger"/>.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    /// <exception cref="ArgumentException">Thrown when an unsupported logger type is provided.</exception>
+    public static IServiceCollection AddLoggingServices(this IServiceCollection services, LoggerType loggerType = LoggerType.FileLogger)
+    {
+        switch (loggerType)
+        {
+            case LoggerType.FileLogger:
+                services.AddSingleton<ILoggerService, FileLogger>();
+                break;
+            case LoggerType.MsSqlLogger:
+                services.AddSingleton<ILoggerService, MsSqlLogger>();
+                break;
+            default:
+                throw new ArgumentException($"Unsupported logger type: {loggerType}", nameof(loggerType));
+        }
+
+        return services;
+    }
+}
---
+++ src/Core.CrossCuttingConcerns/Logging/Models/LoggerType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Logging.Models;
+
+/// <summary>
+/// Specifies the type of logger to be used for logging operations.
+/// </summary>
+public enum LoggerType
+{
+    /// <summary>
+    /// Uses a file-based logger to write logs to a local or shared file.
+    /// </summary>
+    FileLogger,
+
+    /// <summary>
+    /// Uses a Microsoft SQL Server-based logger to write logs to a database table.
+    /// </summary>
+    MsSqlLogger
+}
